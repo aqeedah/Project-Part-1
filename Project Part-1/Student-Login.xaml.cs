007a76df98cb154c77ef85452c325b5d398f8143
Aqeedah <aqeedah2594@gmail.com>
2024-04-02 19:37:37 -0400

    Add project files.

---
+++ Project Part-1/Student-Login.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+
+namespace Project_Part_1
+{
+    /// <summary>
+    /// Interaction logic for Student_Login.xaml
+    /// </summary>
+    public partial class Student_Login : Window
+    {
+        public Student_Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Submit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Project Part-1/question.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Project_Part_1
+{
+    /// <summary>
+    /// Interaction logic for question.xaml
+    /// </summary>
+    public partial class question : Window
+    {
+        public question()
+        {
+            InitializeComponent();
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
